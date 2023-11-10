dd727b73286af3a5125d51884b5ffaf02b0c1c9c
Md. Tamim Iqbal <einsteintalbert167@gmail.com>
2023-11-10 18:30:16 +0600

    Result Sheet

---
+++ Marksheet.py
@@ -0,0 +1,16 @@
+#Marksheet
+mark=int(input("Enter the mark:"))
+if mark>=80:
+    print("A+")
+elif mark>=70:
+    print("A")
+elif mark>=60:
+    print("A-")
+elif mark>=50:
+    print("C")
+elif mark>=40:
+    print("D")
+elif mark>=33:
+    print("B")
+else :
+    print("F")
